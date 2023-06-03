104af6252b098579fda785ebd3a2ef348fcb29d9
Ibrahima Diallo <ibrahimadiallo@Ibrahimas-MBP.ht.home>
2023-06-03 08:33:07 -0400

    read me file added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # work-day-scheduler
-This is a work day scheduler to help employees with a busy schedule to manage successfully their day to day job!
+
+## Description
+
+This is a work day scheduler to help employees with a busy schedule to manage successfully their day to day job! I used color to differentiate the moment the red color means past, the yellow is the current hour and the green is the futur time.
+when you click on save buttun it will display "your entry has been saved" and at the bottom of the page it will display "made with some love by Ibrahima Diallo" I love this feature. It display the current Date & Time on the page. 
+
+
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// Going to w rap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var localSettings = {};
@@ -10,7 +10,7 @@ $(function () {
   //this function change the hour color of each time block
   function hourColor() {
     $('.time-block').each(function () {
-      const blockHour = parseInt(this.id);
+      var blockHour = parseInt(this.id);
       $(this).toggleClass('past', blockHour < currentHour);
       $(this).toggleClass('present', blockHour === currentHour);
       $(this).toggleClass('future', blockHour > currentHour);
@@ -21,17 +21,33 @@ $(function () {
 
   function userInput() {
     $('.saveBtn').on('click', function () {
-      const key = $(this).parent().attr('id');
-      const value = $(this).siblings('.description').val();
+      var key = $(this).parent().attr('id');
+      var value = $(this).siblings('.description').val();
       localStorage.setItem(key, value);
+
+      $('#saved').addClass('show');
+      
+      setTimeout(function(){
+        $('#saved').removeClass('show');
+
+      }, 5000)
+
+// I added this function to display my credit at the bottom of the page I LOVE IT.
+      $('#foot').addClass('show');
+      
+      setTimeout(function(){
+        $('#foot').removeClass('show');
+
+      }, 8000)
+      
     });
   }
 
-  //lets change the color
+  //lets change the color 
 
   function changeColor() {
     $('.time-block').each(function () {
-      const blockHour = parseInt(this.id);
+      var blockHour = parseInt(this.id);
       if (blockHour == currentHour) {
         $(this).removeClass('past future').addClass('present');
       } else if (blockHour < currentHour) {
@@ -46,18 +62,19 @@ $(function () {
   //Save the user input from localStorage
 
   $('.time-block').each(function () {
-    const key = $(this).attr('id');
-    const value = localStorage.getItem(key);
+    var key = $(this).attr('id');
+    var value = localStorage.getItem(key);
     $(this).children('.description').val(value);
+    
   });
 
-  //the display of current date and time 
+  //this function display the  current date and time 
 
   function updateTime() {
-    const dateElement = $('#date');
-    const timeElement = $('#time');
-    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
-    const currentTime = dayjs().format('hh:mm:ss A');
+    var dateElement = $('#date');
+    var timeElement = $('#time');
+    var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    var currentTime = dayjs().format('hh:mm:ss A');
     dateElement.text(currentDate);
     timeElement.text(currentTime);
   }
@@ -65,8 +82,8 @@ $(function () {
   hourColor();
   userInput();
   changeColor();
-
-  setInterval(updateTime, 1000);
+//this will show the time live with deduction by two seconds
+  setInterval(updateTime, 2000);
 
 });
 
